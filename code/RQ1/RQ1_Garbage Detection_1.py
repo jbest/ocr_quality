503554bce036697110d5dee94b574e95b8811381
satish7655 <77863313+satish7655@users.noreply.github.com>
2021-04-29 19:04:45 -0400

    Add files via upload

---
+++ code/RQ1/RQ1_Garbage Detection_1.py
@@ -0,0 +1,24 @@
+# Garbage Detecting using nostril
+
+from nostril import nonsense
+from nltk.tokenize import word_tokenize
+from nltk.corpus import words
+
+# please refer to the RQ1 combined file where all txt files were combined and saved as combined.csv
+file1 = "combined.csv"
+
+with open(file1, "r+") as file:
+    text = file.read()
+    words = word_tokenize(text)
+    print(words)
+
+# the minimum requirement to check the garbage word length is 6
+for word in words:
+        if (len(word)<6):
+            continue
+        if nonsense(word):
+            print(word,"is a nonsense word")
+        else:
+            print(word, "is a real word")
+
+
