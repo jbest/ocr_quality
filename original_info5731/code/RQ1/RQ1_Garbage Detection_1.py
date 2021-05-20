f768987b3a530e25d8379b0d669bab7a7aec91b2
Jason Best <jbest@brit.org>
2021-05-20 13:21:31 -0500

    moved original code and data from INFO 5731 project

--- original_info5731/code/RQ1/RQ1_Garbage Detection_1.py
+++ original_info5731/code/RQ1/RQ1_Garbage Detection_1.py
@@ -14,11 +14,17 @@ with open(file1, "r+") as file:
 
 # the minimum requirement to check the garbage word length is 6
 for word in words:
-        if (len(word)<6):
-            continue
+    if (len(word)<7):
+        #print(word)
+        continue
+    try:
         if nonsense(word):
             print(word,"is a nonsense word")
         else:
-            print(word, "is a real word")
+            #print(word, "is a real word")
+            pass
+    except Exception as e:
+        pass
+        #print('Word too short:', len(word), e)
 
-
+print('done')
